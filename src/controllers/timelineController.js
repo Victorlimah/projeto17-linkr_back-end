4c1cad4dfe7fc0209a64cf7e1bfaa2558f50e1e5
Matheus-daSilva <matheus2001silva@live.com>
2022-06-17 13:25:26 -0300

    fix: urlMetadata response

--- src/controllers/timelineController.js
+++ src/controllers/timelineController.js
@@ -10,28 +10,41 @@ dotenv.config();
 export async function Timeline(req, res) {
     const postsArray = []
     const options = {
-        descriptionLength: 750
+        descriptionLength: 700
     }
     try {
         const infos = await getPosts();
-        for(let info of infos.rows) {
-            const metadata = await urlMetadata(info.link, options)
-            const publicationsInfos = {
-                username: info.username,
-                picture: info.picture,
-                link: info.link,
-                description: info.description,
-                linkPicture: metadata.title,
-                linkTilte: metadata.title,
-                linkDescription: metadata.description
-            }
-            postsArray.push(publicationsInfos)
-        }
+        for (let info of infos.rows) {
+            try {
+                const response = await urlMetadata(info.link, options)
+                const publicationsInfos = {
+                    username: info.username,
+                    picture: info.picture,
+                    link: info.link,
+                    description: info.description,
+                    linkPicture: response.picture,
+                    linkTilte: response.title,
+                    linkDescription: response.description
+                }
+                postsArray.push(publicationsInfos)
 
-        res.status(201).send(postsArray)
+            } catch (e) {
+                const publicationsInfos = {
+                    username: info.username,
+                    picture: info.picture,
+                    link: info.link,
+                    description: info.description,
+                    linkPicture: undefined,
+                    linkTilte: undefined,
+                    linkDescription: undefined
+                }
+                postsArray.push(publicationsInfos)
+            }
+        }
+        res.status(200).send(postsArray)
     } catch (err) {
-        console.log(chalk.red(`ERROR: ${err.message}`))
-        res.status(500).send(err.message)
+        console.log(chalk.red(`ERROR: ${err}`))
+        res.status(500).json(err)
     }
 }
 
@@ -43,7 +56,7 @@ export async function PostUrl(req, res) {
         const post = await postPosts(url, description, id);
         const hashtags = extractHashtags(description);
         console.log(hashtags)
-        if (hashtags?.length > 0) 
+        if (hashtags?.length > 0)
             await addHashtag(post.rows[0].id, hashtags);
         res.status(201).send("Url posted succesfully");
     } catch (err) {
